3d476dac98e92eaae0cdd85f32c756e46815cd7f
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-08-16 19:54:22 -0600

    fixing 3d camera rotation

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Backdrop = () => {
+  return (
+    <div>Backdrop</div>
+  )
+}
+
+export default Backdrop
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,10 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
 
-const Canvas = () => {
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+import { Back } from "gsap";
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+        <Environment preset="city" />
+
+        <CameraRig>
+        {/*   <Backdrop /> */}
+          <Center>
+            <Shirt />
+          </Center>
+        </CameraRig>
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
