fdcc50cda26fe6227d1940233b004369414ba996
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-09-12 14:58:45 -0600

    test

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pm2 stop threadit_ai
-rm -rf threadit_ai
-mkdir -p ~/threadit_ai/server
-echo "OPENAI_API_KEY=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" | tee ~/threadit_ai/server/.env
-chmod 600 ~/threadit_ai/server/.env
